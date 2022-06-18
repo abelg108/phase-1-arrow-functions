183ba505dab87ebef3d8400bc867a9b8a2cd4573
Abel Bokure <abel.ngt@gmail.com>
2022-06-18 15:14:09 -0400

    commit tree 1cb608c9cd0b76dc05d10d3e2c3f7209bcbf097c

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide(){
+    return 2000 / 100
+}
+
+const square = (parameter1) => {return parameter1 * parameter1 }
+
+const add = (parameter1,parameter2) => {return parameter1 + parameter2}
